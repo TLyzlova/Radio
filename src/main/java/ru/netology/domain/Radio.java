3c47118df6cdc16c369a68feda9107a12b40358e
Tatiana Lyzlova <yatanita@gmail.com>
2021-05-17 13:49:43 +0300

    Radio and RadioTest created

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class Radio {
+    int numberOfStation;
+    int volumeOfSound;
+
+
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+
+    public void shouldCreate(){
+        Radio radio = new Radio();
+
+    }
+}
