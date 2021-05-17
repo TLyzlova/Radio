c3189a200cedfd8854506abb161e800704c35799
Tatiana Lyzlova <yatanita@gmail.com>
2021-05-17 17:38:37 +0300

    Get and set created

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,8 +1,110 @@
 package ru.netology.domain;
 
 public class Radio {
-    int numberOfStation;
-    int volumeOfSound;
+
+    private int currentVolume;
+    private int currentStation;
+    private int maxStation ;
+    private int minStation;
+    private int maxVolume;
+    private int minVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
 
 
+        public int getCurrentStation() {
+            return currentStation;
+        }
+
+        public void setCurrentStation(int currentStation){
+            if (currentStation > maxStation) {
+                currentStation = minStation;
+            }
+            if (currentStation < minStation) {
+                currentStation = maxStation;
+            }
+            this.currentStation = currentStation;
+        }
+
+        public int getMaxStation() {
+            return maxStation;
+        }
+
+        public void setMaxStation(int maxStation){
+            this.maxStation = maxStation;
+        }
+
+        public int getMinStation() {
+            return minStation;
+        }
+
+        public void setMinStation(int minStation){
+            this.minStation = minStation;
+        }
+
+        public int getMaxVolume() {
+            return maxVolume;
+        }
+
+        public void setMaxVolume(int maxVolume){
+            this.maxVolume = maxVolume;
+        }
+
+        public int getMinVolume() {
+            return minVolume;
+        }
+
+        public void setMinVolume(int minVolume){
+            this.minVolume = minVolume;
+        }
+
+        public void increaseVolume() {
+            if (currentVolume < 10) {
+                currentVolume = currentVolume + 1;
+            }
+        }
+
+//    public void decreaseVolume() {
+//        if (currentVolume == maxVolume) {
+//            currentVolume = currentVolume - 1;
+//        }
+//    }
+
+        public void maxVolume() {
+            if (currentVolume >= 10) {
+                currentVolume = 10;
+            }
+        }
+
+        public void minVolume() {
+            if (currentVolume <= 0) {
+                currentVolume = 0;
+            }
+        }
+
+        public void nextStation() {
+            if (currentStation > maxStation) {
+                currentStation = minStation;
+            }
+        }
+        public void prevStation() {
+            if (currentStation < minStation) {
+                currentStation = maxStation;
+            }
+        }
+    }
 }
+
+
+
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -2,12 +2,32 @@ package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RadioTest {
 
     @Test
-
     public void shouldCreate(){
         Radio radio = new Radio();
 
     }
+
+//    @Test
+//    public void changeCurrentStation(){
+//        Radio radio = new Radio();
+//        int expected = 6;
+//        assertEquals(6, radio.getCurrentStation());
+//        radio.setCurrentStation(7);
+//        assertEquals(6, radio.getCurrentStation());
+
+
 }
+//    @Test
+//    public void changeCurrentVolume(){
+//        Radio radio = new Radio();
+//        int expected = 1;
+//        assertEquals(1, radio.getCurrentVolume());
+//        radio.setCurrentVolume(expected);
+//        assertEquals(expected, radio.getCurrentVolume());
+
+
